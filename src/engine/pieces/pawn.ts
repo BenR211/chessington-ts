67a160197392a059e0da3475076d1f00315e88fc
Ben Remmer <Ben.Remmer@softwire.com>
2024-06-26 12:09:31 +0100

    corrected pawns

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -1,13 +1,23 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-
+import Square from '../../../src/engine/square';
 export default class Pawn extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let availableMove = [];
+        let currentSqr = board.findPiece(this);
+        if (this.player === Player.WHITE){
+            availableMove.push(new Square(currentSqr.row +1, currentSqr.col));
+        }
+        else {
+            availableMove.push(new Square(currentSqr.row - 1, currentSqr.col));
+        }
+
+        
+        return availableMove;
     }
 }
