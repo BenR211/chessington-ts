68572a72aae4b897d70949ddcc6d8db196c492db
Ben Remmer <Ben.Remmer@softwire.com>
2024-06-26 12:21:41 +0100

    pawns can move two squares on the first move

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -10,10 +10,17 @@ export default class Pawn extends Piece {
     public getAvailableMoves(board: Board) {
         let availableMove = [];
         let currentSqr = board.findPiece(this);
+        
         if (this.player === Player.WHITE){
+            if (currentSqr.row === 1){
+                availableMove.push(new Square(currentSqr.row +2, currentSqr.col));
+            }
             availableMove.push(new Square(currentSqr.row +1, currentSqr.col));
         }
         else {
+            if (currentSqr.row === 6){
+                availableMove.push(new Square(currentSqr.row - 2, currentSqr.col));
+            }
             availableMove.push(new Square(currentSqr.row - 1, currentSqr.col));
         }
 
