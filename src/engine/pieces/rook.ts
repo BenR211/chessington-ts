14861e5006a289c8d0af27f0ed4995268713a589
Ben Remmer <Ben.Remmer@softwire.com>
2024-06-26 14:23:00 +0100

    implemented rook striaght line movement

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,13 +1,26 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-
+import Square from '../square';
 export default class Rook extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves = [];
+        let currrentSqr = board.findPiece(this);
+        let currentRow = currrentSqr.row;
+        let currentCol = currrentSqr.col;
+        for (let i = 0; i < 8; i ++){
+            if (i === currentRow){continue;}
+            moves.push(new Square(i, currentCol));
+        }
+        for (let i = 0; i < 8; i++){
+            if (i === currentCol){continue;}
+            moves.push(new Square(currentRow, i));
+            
+        }
+        return moves;
     }
 }
