9ea9e211cb1b80f820b949ff8a3f0c5c44577827
Ben Remmer <Ben.Remmer@softwire.com>
2024-06-26 15:14:33 +0100

    moving the king

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,13 +1,24 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
-
+import Square from '../../../src/engine/square';
 export default class King extends Piece {
     public constructor(player: Player) {
         super(player);
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        let moves:Square[] = []
+        let currentSqr = board.findPiece(this);
+
+        for (let row = currentSqr.row - 1; row <= currentSqr.row +1; row ++ ){
+            for (let col = currentSqr.col - 1; col <= currentSqr.col + 1; col ++){
+                if (!(currentSqr.row === row && currentSqr.col === col)){
+                    moves.push(new Square(row, col));
+                }
+            }
+        }
+
+        return moves;
     }
 }
